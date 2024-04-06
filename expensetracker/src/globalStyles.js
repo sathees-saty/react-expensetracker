2b2567483fff807d3000b279b412fdf057d55e14
Sathees Kumar K <65384711+sathees-saty@users.noreply.github.com>
2024-04-06 09:38:41 +0530

    Add files via upload

---
+++ expensetracker/src/globalStyles.js
@@ -0,0 +1,16 @@
+// FileName: globalStyles.js 
+
+import { createGlobalStyle } from "styled-components"; 
+
+
+const GlobalStyles = createGlobalStyle` 
+	*{ 
+		margin: 0; 
+		padding: 0; 
+		box-sizing: border-box; 
+		//font-family: 'Poppins', sans-serif; 
+		//font-family: "Times New Roman", Times, serif;
+		font-family: Arial, Helvetica, sans-serif;
+	} 
+` 
+export default GlobalStyles;
